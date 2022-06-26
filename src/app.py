2f872eb903e47aadfe1a8f8707ec7d86a6b706cc
RominaGonella <rominagonfur@gmail.com>
2022-06-26 18:58:49 +0000

    actualizo

--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
+# desde la consola: pip install tweepy
+
+# librerias
 import os
 from dotenv import load_dotenv
+import tweepy
+import requests
 
-# load the .env file variables
+# cargo .env
 load_dotenv()
 
+# cargo datos de acceso desde archivo .env
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+bearer_token = os.environ.get('BEARER_TOKEN')
 
-# your app code here
+# 
+tweepy.Client()
+
